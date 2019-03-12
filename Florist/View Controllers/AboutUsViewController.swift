94c0cacf61b6d7d80732f77091547cbb974bf8f9
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-12 11:53:56 -0400

    Tell a friend send email functionality complete

--- Florist/View Controllers/AboutUsViewController.swift
+++ Florist/View Controllers/AboutUsViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MessageUI
+import ProgressHUD
 
-class AboutUsViewController: UIViewController {
+class AboutUsViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     @IBOutlet weak var aboutUsTextView: UITextView!
     
@@ -21,6 +23,37 @@ class AboutUsViewController: UIViewController {
     
 
     @IBAction func tellAFriendBarButtonPressed(_ sender: UIBarButtonItem) {
+ 
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+           // mail.setToRecipients(["test@test.test"])
+            mail.setSubject("Check out this iPhone App")
+            mail.setMessageBody("<p>I found a really cool app which lets you send the freshest flowers from your iPhone to anyone in Miami FL.<p><br /><br /><p>Check it out im sure you will find it cool as well", isHTML: true)
+            present(mail, animated: true, completion: nil)
+        } else {
+            // give feedback to the user
+            ProgressHUD.showError("Something went wrong. Please try again later and make sure you are connected to the internet.")
+        }
+        
     }
+    
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result.rawValue {
+        case MFMailComposeResult.cancelled.rawValue:
+            print("Cancelled")
+        case MFMailComposeResult.saved.rawValue:
+            print("Saved")
+        case MFMailComposeResult.sent.rawValue:
+            print("Sent")
+        case MFMailComposeResult.failed.rawValue:
+            print("Error: \(String(describing: error?.localizedDescription))")
+        default:
+            break
+        }
+        controller.dismiss(animated: true, completion: nil)
+    }
+        
     
 }
